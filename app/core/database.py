c19fbf007acb6a4e4d275513edd3c185844f5365
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2025-06-07 19:50:13 -0400

    Agrega configuracion de credenciales mediante variables
    Esto permitirá al usuario poder usar sus credenciales del entorno de MySQL con el cual dispone para la base de datos de Sakila.
    
    Cierra #4

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:Admin1234@localhost:3306/sakila"
+from dotenv import load_dotenv
+
+load_dotenv()
+db_host: str = os.getenv("DB_HOST", "localhost")
+db_port: int = int(os.getenv("DB_PORT", 3306))
+db_name: str = os.getenv("DB_NAME", "sakila")
+db_user: str | None = os.getenv("DB_USER")
+db_password: str | None = os.getenv("DB_PASSWORD")
+
+DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
